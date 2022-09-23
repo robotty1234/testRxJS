13cefc215a9257cfe2506898f45781d582b7c009
robotty1234 <67456219+robotty1234@users.noreply.github.com>
2022-09-23 22:57:14 +0900

    3 times

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var Rx = require('rx');
 
-//イベントの元となる配列
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-//配列から配列の要素ごとに10回イベントが起きる発生源を作成する。
+//配列から配列の要素ごとに10回イベントが起きるストリームを作成する。
 var source = Rx.Observable.fromArray(array);
 
-//イベントを受け取るよう指定する(配列の要素を10回受け取る)
-//これでイベントの処理の流れが決まった。
-source.subscribe(function (x) {
+//イベントの発生源から3の倍数だけを取り出すフィルターを追加
+//フィルターもまたイベント源
+var filtered = source.filter(function (x) {
+  //3の倍数時だけOKを返却。
+  if (x % 3 == 0) {
+    return true;
+  }
+});
+
+//filterされたイベント源からイベントを受け取るよう指定してストリームが完成
+filtered.subscribe(function (x) {
   console.log(x);
 });
