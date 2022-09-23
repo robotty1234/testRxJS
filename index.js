df04a7f05bbd83d0e12e49a80d05ec3c7d298183
robotty1234 <67456219+robotty1234@users.noreply.github.com>
2022-09-23 22:57:40 +0900

    japanese value

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //配列から配列の要素ごとに10回イベントが起きるストリームを作成する。
 var source = Rx.Observable.fromArray(array);
 
-//イベントの発生源から3の倍数だけを取り出すフィルターを追加
-//フィルターもまたイベント源
-var filtered = source.filter(function (x) {
-  //3の倍数時だけOKを返却。
-  if (x % 3 == 0) {
-    return true;
-  }
+//イベントの発生源からの内容を変換するmapを追加
+var mapped = source.map(function (x) {
+  var ret = [
+    '-',
+    'いち',
+    'に',
+    'さん',
+    'よん',
+    'ご',
+    'ろく',
+    'なな',
+    'はち',
+    'きゅう',
+    'じゅう',
+  ];
+  return ret[x];
 });
 
-//filterされたイベント源からイベントを受け取るよう指定してストリームが完成
-filtered.subscribe(function (x) {
+//filterされたストリームからイベントを受け取るよう指定する
+mapped.subscribe(function (x) {
   console.log(x);
 });
